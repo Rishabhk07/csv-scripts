49f2f346ec2daa87b9765bbac77d25ddfa70c4bb
Rishabh Khanna <rishabhkhanna777@gmail.com>
2018-03-28 05:19:27 -0400

    script csv to db
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- script_csv-to-db.js
+++ script_csv-to-db.js
@@ -1,12 +1,25 @@
 /**
  * Created by rishabhkhanna on 28/03/18.
  */
-const sworn = require('sworn');
-const csvFilePath = "";
+const sworn = require('sworm');
+const csvFilePath = "./categories.csv";
 const csv = require('csvtojson');
+const url = "oracle://mmehar:meharsingh@oracle.cise.ufl.edu:1521/orcl&maxRows=100000&pool=true";
+let db = sworn.db({
+    driver: 'oracle',
+    config: {
+        user: 'mmehar',
+        password: 'meharsingh',
+        host: 'oracle.cise.ufl.edu',
+        sid: 'orcl'
+    }
+});
+var categories = db.model({table: 'categories'});
 
 csv().fromFile(csvFilePath).on('json', (jsonObj)=>{
     console.log(jsonObj);
+    var saveThis = categories(jsonObj);
+    saveThis.save();
 }).on('done', (error)=>{
     console.log(error);
 });
